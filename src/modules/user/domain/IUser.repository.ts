113c26d05d24dd1b4c04e47a82d03abadc27ce02
berkanioussama <berkanioussama@gmail.com>
2025-12-04 19:01:20 +0100

    add auth utils

--- src/modules/user/domain/IUser.repository.ts
+++ src/modules/user/domain/IUser.repository.ts
@@ -1,11 +1,11 @@
-import { User } from "./user.entity";
+import { User } from "./user.entity"
 
 export interface IUserRepository {
-  add(user: User): Promise<User>;
-  findById(id: string): Promise<User | null>;
-  findByEmail(email: string): Promise<User | null>;
-  findByAuthProviderId(authProviderId: string): Promise<User | null>;
-  findAll(): Promise<User[]>;
-  edit(user: User): Promise<User>;
-  remove(id: string): Promise<void>;
+  add(user: User): Promise<User>
+  findById(id: string): Promise<User | null>
+  findByEmail(email: string): Promise<User | null>
+  findByAuthProviderId(authProviderId: string): Promise<User | null>
+  findAll(): Promise<User[]>
+  edit(user: User): Promise<User>
+  remove(id: string): Promise<void>
 }
---
+++ src/shared/api/utils/auth-utils.ts
@@ -0,0 +1,14 @@
+import { getAuth } from "@hono/clerk-auth";
+import { Context } from "hono";
+import { UserAuthProviderRepository } from "../../../modules/user/infrastructure/user-auth-provider.repository";
+import { ApiResponse } from "./api-response";
+
+const userAuthProviderRepository = new UserAuthProviderRepository()
+
+export async function isCorrectUser(c: Context) {
+    const auth = getAuth(c)
+    const userId = c.req.param('userId')
+    if(!auth?.userId) return null
+    const user = await userAuthProviderRepository.findByAuthProviderId(auth?.userId)
+    if(user?.id !== userId) return c.json(ApiResponse('Unothorized to access this resource'), 401)
+}
