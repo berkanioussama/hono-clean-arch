500dd2184a5f3af909073e835997d1777f07564f
Berkani oussama ala eddine <berkanioussama@gmail.com>
2026-01-24 04:15:46 +0100

    Fix user service user ID value object

--- src/modules/user/application/service/auth.service.ts
+++ src/modules/user/application/service/auth.service.ts
@@ -1,21 +1,21 @@
 import { IUserRepo } from "../../domain/IUser.repo";
 import { Role } from "../../domain/user.entity";
+import { ProviderId } from "../../domain/valueObject";
+import { UnauthorizedError } from "../../../../shared/domain/errors";
 
 export class AuthService {
   constructor(private userRepo: IUserRepo ) {}
 
   async isAdmin(providerId: string): Promise<boolean> {
-    if (!providerId) {
-      throw new Error("No user provider id provided");
-    }
-    const user = await this.userRepo.findByProviderId(providerId);
+    const providerIdVO = ProviderId.create(providerId)
+    const user = await this.userRepo.findByProviderId(providerIdVO);
     return user?.role === Role.ADMIN;
   }
 
   async requireAdmin(providerId: string): Promise<void> {
     const isAdmin = await this.isAdmin(providerId);
     if (!isAdmin) {
-      throw new Error("Admin access required");
+      throw new UnauthorizedError("Admin access required");
     }
   }
 }
--- src/modules/user/application/service/user.service.ts
+++ src/modules/user/application/service/user.service.ts
@@ -1,10 +1,13 @@
 import { IUserRepo } from "../../domain/IUser.repo";
 import { User } from "../../domain/user.entity";
+import { ProviderId } from "../../domain/valueObject";
 
 export class UserService {
   constructor(private userRepo: IUserRepo) {}
 
   async findUserByProviderId(providerId: string): Promise<User | null> {
-    return this.userRepo.findByProviderId(providerId);
+    const providerIdVO = ProviderId.create(providerId)
+
+    return this.userRepo.findByProviderId(providerIdVO);
   }
 }
